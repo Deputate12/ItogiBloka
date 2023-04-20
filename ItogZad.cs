cd57eb1357d24feae1e710f047b05d106b29bf7a
Deputate12 <120495930+Deputate12@users.noreply.github.com>
2023-04-20 16:00:21 +0300

    Update ItogZad.cs

--- ItogZad.cs
+++ ItogZad.cs
@@ -1,1 +1,12 @@
-
+string[] array = {"dog","cat","hello","life","world"};
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.Write(string.Join(Environment.NewLine, result, 0, realSize));
